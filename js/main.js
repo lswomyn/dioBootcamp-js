ddfd85abbcb97a535b3ebd6d8b8bc0c0e76896ec
Laura Simoni <laura.simoni@gmail.com>
2020-11-09 21:21:55 +0000

    JS intro - class

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+alert(soma(5,10));
+
+/*var d = Date();
+alert(d);
+
+
+
+var count = 0;
+while (count<5) {
+    console.log(count);
+    count++;
+}
+
+
+/*
+var idade = prompt("Qual a sua idade?");
+
+if (idade >= 18) {
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade!")
+}
+/*
+var fruta = {nome: "Maçã", cor:"Vermelha"};
+console.log(fruta);
+console.log(fruta.cor);
+
+var lista = ["cachorro", "gato", "papagaio"];
+console.log(lista);
+lista.push("periquito");
+console.log(lista);
+lista.pop();
+console.log(lista.join(" - "));
+
+var name = "Laura Simoni";
+var age = 35;
+var n = 10;
+alert(name + " is " + age + " years old");
+console.log(age * n);
+console.log(age+n);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+*/
