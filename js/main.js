59ec558917fa86003596991878302da1611cccb3
Laura Simoni <laura.simoni@gmail.com>
2020-11-10 06:35:38 +0000

    onchange, onmouseover, onclick, onmouseout,onload, redirect with window.open and window.location

--- js/main.js
+++ js/main.js
@@ -1,47 +1,31 @@
-function soma(n1, n2) {
-    return n1 + n2;
-}
-
-alert(soma(5,10));
-
-/*var d = Date();
-alert(d);
-
-
-
-var count = 0;
-while (count<5) {
-    console.log(count);
-    count++;
+function clicou() {
+    document.getElementById("thanks").innerHTML = "<b>Thanks for clicking</b>";
+    console.log(document.getElementById("thanks"));
 }
 
 
-/*
-var idade = prompt("Qual a sua idade?");
-
-if (idade >= 18) {
-    alert("Maior de idade")
-}else{
-    alert("Menor de idade!")
+function redirect() {
+    window.open("https://www.freecodecamp.org/learn/");
+    //abre em outra aba
+    window.location.href = "https://www.freecodecamp.org/learn/";
+    //abre na mesma aba
 }
-/*
-var fruta = {nome: "Maçã", cor:"Vermelha"};
-console.log(fruta);
-console.log(fruta.cor);
 
-var lista = ["cachorro", "gato", "papagaio"];
-console.log(lista);
-lista.push("periquito");
-console.log(lista);
-lista.pop();
-console.log(lista.join(" - "));
+function change(element) {
+    //document.getElementById("mouseover").innerHTML = "Naughty :P"
+    element.innerHTML = "Naughty :P";
+}
 
-var name = "Laura Simoni";
-var age = 35;
-var n = 10;
-alert(name + " is " + age + " years old");
-console.log(age * n);
-console.log(age+n);
-console.log(name.toLowerCase());
-console.log(name.toUpperCase());
-*/
+function back(element) {
+    //document.getElementById("mouseover").innerHTML = "Pass over here"
+    element.innerHTML = "Pass over here";
+}
+
+function load() {
+    alert("Page loaded");
+}
+
+function choose(element) {
+    console.log(element.value)
+    
+}
